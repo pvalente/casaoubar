0c771b36ca40f7daa8cca34520b4447294baece7
Pedro Valente <pedro.valente@gmail.com>
2010-03-21 01:06:28 -0300

    its alive

--- main.py
+++ main.py
@@ -19,20 +19,50 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 from foursquare import foursquare
+import yql
 
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
     
     f = foursquare.Api()
-    venues = f.get_venues('-23.669957016335974', '-46.69841766357422', q='bar') 
+    #venues = f.get_venues('-23.669957016335974', '-46.69841766357422', q='bar') 
+    
+    y = yql.Public()
+    query_km = "select * from html where xpath=\"//*[@id='lentidao']/strong\" and url='http://cetsp1.cetsp.com.br/monitransmapa/agora/'"
+
+    km = y.execute(query_km)
+    
+    query_trend = 'select title from html where xpath=\'//*[@id="tendencia"]/img\' and url="http://cetsp1.cetsp.com.br/monitransmapa/agora/"'
+    
+    trend = y.execute(query_trend)
+        
+    
     template_values = {
-             'url': 'url',            
+             'km': km.rows,         
+             'trend': trend.rows['title'],
+             'decision' : decide(km, trend),         
             }
         
-    path = os.path.join(os.path.dirname(__file__), 'templates%s.html' % "/casaoubar")
+    path = os.path.join(os.path.dirname(__file__), 'templates/casaoubar.html')
     self.response.out.write(template.render(path, template_values))
 
+def decide(km, trend):
+    km = int(km.rows)
+    trend = trend.rows['title']
+    if trend == "BAIXA":
+        if km < 100:
+            return "casa"
+        return "bar"
+    elif trend == "ESTAVEL":
+        if km < 90:
+            return "casa"
+        return "bar"
+    elif trend == "ALTA":
+        if km < 70:
+            return "casa"
+        return "bar"
+    
 
 def main():
   application = webapp.WSGIApplication([('/', MainHandler)],
